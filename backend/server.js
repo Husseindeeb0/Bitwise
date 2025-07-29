bcb844d29655232a554e9a70d8514a8a14475d2b
Hussein Deeb <deebh776@gmail.com>
2025-07-29 13:47:53 +0300

    Add docker configuration

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const PORT = process.env.PORT || 5000;
-require("dotenv").config();
+const dotenv = require("dotenv");
 const express = require("express");
 const cors = require("cors");
 const corsOptions = require("./config/corsoptions");
@@ -7,6 +7,8 @@ const connectDB = require("./config/dbconnect");
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 const announcementsRoutes = require("./routes/announcementsRoutes");
+
+dotenv.config();
 
 const app = express();
 connectDB();
