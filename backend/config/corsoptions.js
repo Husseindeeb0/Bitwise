20731ac5613be41ceada6377f632465e952434e1
Hussein Deeb <deebh776@gmail.com>
2025-07-29 13:49:32 +0300

    Add localhost of docker to cors options

--- backend/config/corsoptions.js
+++ backend/config/corsoptions.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   "https://bitwiseclub.com",
   "https://frontend-service-261191369874.us-central1.run.app",
   "http://localhost:5173",
+  "http://localhost:3000",
   "https://bitwise0.netlify.app",
 ];
 
