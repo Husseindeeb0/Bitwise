9569764cf515461e457e1893de5a8e7654bcf382
Hussein Deeb <deebh776@gmail.com>
2025-08-05 22:09:59 +0300

    modify imageKit config file

--- backend/config/imageKit.js
+++ backend/config/imageKit.js
@@ -2,10 +2,6 @@ const ImageKit  = require("imagekit");
 const dotenv = require("dotenv");
 dotenv.config();
 
-if (!process.env.IMAGEKIT_PUBLIC_KEY || !process.env.IMAGEKIT_PRIVATE_KEY || !process.env.IMAGEKIT_URL_ENDPOINT) {
-  throw new Error("Missing ImageKit environment variables.");
-}
-
 const imagekit = new ImageKit({
   publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
